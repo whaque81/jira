495ec0c6af2d8bf8b0a1c1fcf5e01de7c3f5efd2
Wasim Haque <whaque@Wasims-MacBook-Pro.local>
2018-03-09 23:55:30 -0500

    formatting

--- src/main/java/com/pk/eqa/jira/DefaultTrustManager.java
+++ src/main/java/com/pk/eqa/jira/DefaultTrustManager.java
@@ -5,26 +5,26 @@ import javax.security.cert.CertificateException;
 import javax.security.cert.X509Certificate;
 
 public class DefaultTrustManager implements X509TrustManager {
-	
+
 	public void checkClientTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {}
 
-    public void checkServerTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {}
+	public void checkServerTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {}
 
-    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-        return null;
+	public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+		return null;
 
-    }
+	}
 
 	public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType)
 			throws java.security.cert.CertificateException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType)
 			throws java.security.cert.CertificateException {
 		// TODO Auto-generated method stub
-		
+
 	}
-    
+
 }
--- src/main/java/com/pk/eqa/jira/Jira.java
+++ src/main/java/com/pk/eqa/jira/Jira.java
@@ -27,7 +27,7 @@ public class Jira {
 	static String password=ReadProperty.getJiraProperty("Jira.credentials.password");
 	static String url=ReadProperty.getJiraProperty("Jira.instance.url");
 	Logger log = LoggerFactory.getLogger(Jira.class);
-	
+
 	public static void main(String args[]) throws Exception{
 		//authentication();
 		//updateResult();
@@ -36,56 +36,56 @@ public class Jira {
 		Jira jira = new Jira();
 		jira.getAuthorization();
 	}
-	
 
-	
+
+
 	public void getAuthorization() throws Exception{
 		String request = "{\"username\": \""+user+"\",\"password\": \""+password+"\"}";
 		String exception="";
-    	URL Url = new URL(url+"/rest/auth/1/session");
-    	HttpsURLConnection con = openConnection(Url);
-    	con.setRequestProperty("Content-Type", "application/json");
-    	con.setRequestProperty("Accept", "application/json");
-    	con.setRequestMethod("POST");
-    	con.connect();
-    	try{
-	    	OutputStreamWriter writer = new OutputStreamWriter(con.getOutputStream());
-	        writer.write(request);
-	        writer.close();
-	        writer=null;}
-    	catch(Exception e) {
-    		exception=e.getMessage();
-    		log.info("Response code returned="+con.getResponseCode());
-    		log.error("Failed to create a Jira session: "+exception);
-    	}
-    	if(exception.isEmpty()){
-    		log.info("Authorization was successful");
-    	}
-        con.disconnect();
-    }
-	
+		URL Url = new URL(url+"/rest/auth/1/session");
+		HttpsURLConnection con = openConnection(Url);
+		con.setRequestProperty("Content-Type", "application/json");
+		con.setRequestProperty("Accept", "application/json");
+		con.setRequestMethod("POST");
+		con.connect();
+		try{
+			OutputStreamWriter writer = new OutputStreamWriter(con.getOutputStream());
+			writer.write(request);
+			writer.close();
+			writer=null;}
+		catch(Exception e) {
+			exception=e.getMessage();
+			log.info("Response code returned="+con.getResponseCode());
+			log.error("Failed to create a Jira session: "+exception);
+		}
+		if(exception.isEmpty()){
+			log.info("Authorization was successful");
+		}
+		con.disconnect();
+	}
+
 	public HttpsURLConnection openConnection(URL url) throws Exception{
 		HttpsURLConnection con=null;
 		SSLContext ctx = SSLContext.getInstance("TLS");
-	    ctx.init(new KeyManager[0], new TrustManager[] {new DefaultTrustManager()}, new SecureRandom());
-	    SSLContext.setDefault(ctx);
-	    try{
-	    	 con = (HttpsURLConnection)url.openConnection();
-	    }catch(MalformedURLException e){
-	    	log.error("Failed to open https connection: "+e);
-	    }
-	    con.setRequestProperty("User-Agent","Mozilla/5.0 ( compatible ) ");
-	    con.setRequestProperty("Authorization", "Basic " + getAuthentication("whaque","!!Nov1981"));
-	    con.setConnectTimeout(15000);
-    	con.setDoOutput(true);
-        con.setDoInput(true);
-        con.setUseCaches(false);
-        con.setDefaultUseCaches(false);
-        con.setAllowUserInteraction(true);
-    	
+		ctx.init(new KeyManager[0], new TrustManager[] {new DefaultTrustManager()}, new SecureRandom());
+		SSLContext.setDefault(ctx);
+		try{
+			con = (HttpsURLConnection)url.openConnection();
+		}catch(MalformedURLException e){
+			log.error("Failed to open https connection: "+e);
+		}
+		con.setRequestProperty("User-Agent","Mozilla/5.0 ( compatible ) ");
+		con.setRequestProperty("Authorization", "Basic " + getAuthentication("whaque","!!Nov1981"));
+		con.setConnectTimeout(15000);
+		con.setDoOutput(true);
+		con.setDoInput(true);
+		con.setUseCaches(false);
+		con.setDefaultUseCaches(false);
+		con.setAllowUserInteraction(true);
+
 		return con;
 	}
-	
+
 	public  void updateResult(String issueIDorKey, String status) throws Exception{
 		String exception="";
 		String transitionID=getTransitionID(issueIDorKey, status);
@@ -100,25 +100,25 @@ public class Jira {
 			OutputStreamWriter writer = new OutputStreamWriter(con.getOutputStream());
 			writer.write(request);
 			writer.close();
-		    writer=null;} 
+			writer=null;} 
 		catch(Exception e){
 			exception=e.getMessage();
 			log.info("Response code returned="+con.getResponseCode());
 			log.error("Failed to update the status: "+exception);
 		}
 		if(exception.isEmpty()){
-    		log.info("Status of the issue "+issueIDorKey+" was changed to "+status+" successfully");
-    	}
-	    con.disconnect();
-    } 
+			log.info("Status of the issue "+issueIDorKey+" was changed to "+status+" successfully");
+		}
+		con.disconnect();
+	} 
 
 	public static String getAuthentication(String username, String password) {
-	    String auth = Base64.getEncoder().encodeToString((username+":"+password).getBytes());
-	    return auth;
-	 	}
-	
+		String auth = Base64.getEncoder().encodeToString((username+":"+password).getBytes());
+		return auth;
+	}
+
 	public  String getTransitionID(String issueIDorKey, String targetStatus) throws Exception{
-		
+
 		String responseHeaders="";
 		String line="";
 		String transitionID="";
@@ -129,50 +129,50 @@ public class Jira {
 		con.setRequestMethod("GET");
 		try{
 			con.connect();
-		    BufferedReader rd = new BufferedReader (new InputStreamReader(con.getInputStream()));
-		    while ((line = rd.readLine()) != null)
-		    	responseHeaders = responseHeaders + line;
-		    JSONObject jsonobject = new JSONObject(responseHeaders);
-		    int count = jsonobject.getJSONArray("transitions").length();
-		    for(int i=0;i<count;i++){
-		    	String jarray = jsonobject.getJSONArray("transitions").getJSONObject(i).get("name").toString();
-		        if(jarray.toLowerCase().equals(targetStatus.toLowerCase())){
-		        	transitionID=jsonobject.getJSONArray("transitions").getJSONObject(i).get("id").toString();
-		        	break;
-		        }
-		    }
+			BufferedReader rd = new BufferedReader (new InputStreamReader(con.getInputStream()));
+			while ((line = rd.readLine()) != null)
+				responseHeaders = responseHeaders + line;
+			JSONObject jsonobject = new JSONObject(responseHeaders);
+			int count = jsonobject.getJSONArray("transitions").length();
+			for(int i=0;i<count;i++){
+				String jarray = jsonobject.getJSONArray("transitions").getJSONObject(i).get("name").toString();
+				if(jarray.toLowerCase().equals(targetStatus.toLowerCase())){
+					transitionID=jsonobject.getJSONArray("transitions").getJSONObject(i).get("id").toString();
+					break;
+				}
+			}
 		}
-		    catch(Exception e){
-		    	log.info("Response code returned="+con.getResponseCode());
-		    	log.error("Failed to get Trasition ID: "+e.getMessage());
-		    }
-	    con.disconnect();
-	    return transitionID;	
+		catch(Exception e){
+			log.info("Response code returned="+con.getResponseCode());
+			log.error("Failed to get Trasition ID: "+e.getMessage());
+		}
+		con.disconnect();
+		return transitionID;	
 	}
 
 	public void addAttachment(String issueIDorKey, String fullFileName) throws Exception{
 		String exception="";
 		File file=new File(fullFileName);
 		byte[] bFile = Files.readAllBytes(file.toPath());
-        String contentDisposition = "Content-Disposition: form-data; name=\"file\"; filename=\"" + file.getName() + "\"";
- 		String contentType = "Content-Type: application/octet-stream";	    	
+		String contentDisposition = "Content-Disposition: form-data; name=\"file\"; filename=\"" + file.getName() + "\"";
+		String contentType = "Content-Type: application/octet-stream";	    	
 		String BOUNDARY = "*****";
 		String CRLF = "\r\n";		
 		StringBuffer requestBody = new StringBuffer();
- 		requestBody.append("--");
- 		requestBody.append(BOUNDARY);
- 		requestBody.append(CRLF);
- 		requestBody.append(contentDisposition);
- 		requestBody.append(CRLF);
- 		requestBody.append(contentType);
- 		requestBody.append(CRLF);
-	   	requestBody.append(CRLF);
-	   	requestBody.append(new String(bFile));
-	    requestBody.append(CRLF);
 		requestBody.append("--");
-	   	requestBody.append(BOUNDARY);
+		requestBody.append(BOUNDARY);
+		requestBody.append(CRLF);
+		requestBody.append(contentDisposition);
+		requestBody.append(CRLF);
+		requestBody.append(contentType);
+		requestBody.append(CRLF);
+		requestBody.append(CRLF);
+		requestBody.append(new String(bFile));
+		requestBody.append(CRLF);
 		requestBody.append("--");
-	
+		requestBody.append(BOUNDARY);
+		requestBody.append("--");
+
 		URL Url = new URL(url+"/rest/api/2/issue/"+issueIDorKey+"/attachments");
 		HttpsURLConnection con = openConnection(Url);
 		con.setRequestMethod("POST");
@@ -190,8 +190,8 @@ public class Jira {
 			log.error("Failed to upload an attachment: "+exception);
 		}
 		if(exception.isEmpty()){
-    		log.info("Attachment - "+fullFileName+" was added to the issue "+issueIDorKey+" successfully");
-    	}
+			log.info("Attachment - "+fullFileName+" was added to the issue "+issueIDorKey+" successfully");
+		}
 		con.disconnect();
 	}
 }
